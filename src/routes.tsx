b6133bb1b8ba6d7b1c5f1671636432f046dd4c95
gapinsky <gapinski02@gmail.com>
2025-03-18 23:51:15 +0100

    routing profile library wishlist

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,9 @@ import Games from "./my-components/games/Games";
 import GamesByGenre from "./my-components/games/GamesByGenre";
 import GamePage from "./my-components/games/gamePage/GamePage";
 import ErrorPage from "./my-components/general/ErrorPage";
+import ProfilePage from "./my-components/profile/ProfilePage";
+import LibraryPage from "./my-components/mylibrary/LibraryPage";
+import WishlistPage from "./my-components/wishlist/WishlistPage";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +16,18 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Games />,
+      },
+      {
+        path: "profile/:nickname",
+        element: <ProfilePage />,
+      },
+      {
+        path: "library/:nickname",
+        element: <LibraryPage />,
+      },
+      {
+        path: "wishlist/:nickname",
+        element: <WishlistPage />,
       },
       {
         path: "genre/:genre",
