bcb5b2f90da4e115c538a3afc7f51b6446d4d521
gapinsky <gapinski02@gmail.com>
2025-03-18 23:55:52 +0100

    links aside

--- src/my-components/aside/AsideNavList.tsx
+++ src/my-components/aside/AsideNavList.tsx
@@ -1,24 +1,35 @@
 import { GrGift } from "react-icons/gr";
 import { VscLibrary } from "react-icons/vsc";
+import { Link } from "react-router-dom";
 
 const AsideNavList = () => {
   return (
     <ul className="w-[40%] px-2  space-y-8 md:w-fit  lg:px-0 lg:space-y-4 lg:w-full  ">
-      <li className="text-2xl font-bold ">Home</li>
-      <li className="text-lg flex items-center">
-        nickname
-        <img
-          src="/images/noImage.png"
-          alt=""
-          className="ml-2 h-8 rounded-full border"
-        />
+      <li className="text-2xl font-bold ">
+        <Link to="" className="hover:opacity-60">
+          Home
+        </Link>
       </li>
-      <li className="text-lg flex items-center">
-        <VscLibrary className="text-2xl mr-2" />
-        My library
+      <li>
+        <Link to={`profile/nickname`} className="text-lg flex items-center">
+          nickname
+          <img
+            src="/images/noImage.png"
+            alt=""
+            className="ml-2 h-8 rounded-full border"
+          />
+        </Link>
       </li>
-      <li className="text-lg flex items-center">
-        <GrGift className="text-2xl mr-2" /> Wishlist
+      <li>
+        <Link to={`library/nickname`} className="text-lg flex items-center">
+          <VscLibrary className="text-2xl mr-2" />
+          My library
+        </Link>
+      </li>
+      <li>
+        <Link to={`wishlist/nickname`} className="text-lg flex items-center">
+          <GrGift className="text-2xl mr-2" /> Wishlist
+        </Link>
       </li>
     </ul>
   );
