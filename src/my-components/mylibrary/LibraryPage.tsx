afae5b7c30adfb10339fab984936ec65a6cb50cd
gapinsky <gapinski02@gmail.com>
2025-03-20 21:14:40 +0100

    library searching game

--- src/my-components/mylibrary/LibraryPage.tsx
+++ src/my-components/mylibrary/LibraryPage.tsx
@@ -1,15 +1,26 @@
 import reducerContext from "@/context/LibraryWishlistContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import GameThumbnail from "./GameThumbnail";
+import { BsSearch } from "react-icons/bs";
 
 const LibraryPage = () => {
   const { state } = useContext(reducerContext);
-  console.log(state);
+  const [filterInput, setFilterInput] = useState("");
   return (
-    <div className="w-full p-16 ">
-      <p className="text-6xl font-semibold">My Library</p>
+    <div className="w-full p-5 md:p-8 lg:p-16 ">
+      <p className="text-5xl md:text-6xl font-semibold">My Library</p>
       <p className="my-8 text-4xl">
         {`Games: ${state.library.length > 0 ? state.library.length : "0"}`}
+      </p>
+      <p className="w-[95%] md:w-[80%] mx-auto flex justify-center items-center my-8 ">
+        <BsSearch className="text-2xl mr-2  opacity-40" />
+        <input
+          value={filterInput}
+          onChange={(e) => setFilterInput(e.target.value)}
+          type="text"
+          placeholder="Search library..."
+          className="w-full px-2 py-2 text-xl border-b-2 dark:border-neutral-500"
+        />
       </p>
       <div className=" grid grid-cols-1 gap-8 md:grid-cols-2  xl:grid-cols-4 xl:gap-16">
         {state.library.length === 0 && (
@@ -17,16 +28,31 @@ const LibraryPage = () => {
             Your library is empty.
           </p>
         )}
-        {state.library.map((item) => (
-          <GameThumbnail
-            key={item.id}
-            name={item.name}
-            slug={item.slug}
-            background_image={item.background_image}
-            id={item.id}
-            type="library"
-          />
-        ))}
+        {filterInput !== "" &&
+          state.library
+            .filter((item) =>
+              item.name.toLowerCase().includes(filterInput.toLowerCase()))
+            .map((item) => (
+              <GameThumbnail
+                key={item.id}
+                name={item.name}
+                slug={item.slug}
+                background_image={item.background_image}
+                id={item.id}
+                type="library"
+              />
+            ))}
+        {filterInput === "" &&
+          state.library.map((item) => (
+            <GameThumbnail
+              key={item.id}
+              name={item.name}
+              slug={item.slug}
+              background_image={item.background_image}
+              id={item.id}
+              type="library"
+            />
+          ))}
       </div>
     </div>
   );
--- src/my-components/wishlist/WishlistPage.tsx
+++ src/my-components/wishlist/WishlistPage.tsx
@@ -1,14 +1,23 @@
 import reducerContext from "@/context/LibraryWishlistContext";
 import { useContext } from "react";
 import GameThumbnail from "../mylibrary/GameThumbnail";
+import { BsSearch } from "react-icons/bs";
 
 const WishlistPage = () => {
   const { state } = useContext(reducerContext);
   return (
-    <div className="w-full p-16">
-      <p className="text-6xl font-semibold">My Wishlist</p>
+    <div className="w-full p-5 md:p-8 lg:p-16">
+      <p className="text-5xl md:text-6xl font-semibold">My Wishlist</p>
       <p className="my-8 text-4xl">
-        {`Games: ${state.library.length > 0 ? state.library.length : "0"}`}
+        {`Games: ${state.wishlist.length > 0 ? state.wishlist.length : "0"}`}
+      </p>
+      <p className="w-[95%] md:w-[80%] mx-auto flex justify-center items-center my-8 ">
+        <BsSearch className="text-2xl mr-2  opacity-40" />
+        <input
+          type="text"
+          placeholder="Search wishlist..."
+          className="w-full px-2 py-2 text-xl border-b-2 dark:border-neutral-500"
+        />
       </p>
       <div className=" grid grid-cols-1 gap-8 md:grid-cols-2  xl:grid-cols-4 xl:gap-16">
         {state.wishlist.length === 0 && (
